252e82d9c73fdf8a3c7949898c05eb79931eb31b
Chantuu <47988187+Chantuu@users.noreply.github.com>
2023-07-07 17:19:24 +0400

    Implement round game logic

--- game.js
+++ game.js
@@ -1,16 +1,34 @@
 function getComputerChoice() {
     const choice = Math.round(Math.random() * 2) + 1;
-    
+
     switch (choice) {
         case 1:
             return 'Rock';
 
         case 2:
             return 'Paper';
-            
+
         case 3:
-            return 'Scissor';
+            return 'Scissors';
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log('Draw Round');
+    } else if (playerSelection === 'Rock' && computerSelection == 'Paper') {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection === 'Rock' && computerSelection == 'Scissors') {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection === 'Paper' && computerSelection == 'Rock') {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection === 'Paper' && computerSelection == 'Scissors') {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection === 'Scissors' && computerSelection == 'Paper') {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection === 'Scissors' && computerSelection == 'Rock') {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+console.log(playRound(playerChoice, getComputerChoice()));
