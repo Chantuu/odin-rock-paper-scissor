f506e11ca51421e4c2aa894a58a3237043e58ae3
Chantuu <47988187+Chantuu@users.noreply.github.com>
2023-07-10 20:02:07 +0400

    Updated round results for ui response

--- game.js
+++ game.js
@@ -22,19 +22,19 @@ function playRound(playerSelection, computerSelection) {
     computerChoice.innerText = computerSelection;
 
     if (playerSelection === computerSelection) {
-        return 'Draw Round'
+        alert('Draw Round');
     } else if (playerSelection === 'Rock' && computerSelection == 'Paper') {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        alert(`You lose! ${computerSelection} beats ${playerSelection}`);
     } else if (playerSelection === 'Rock' && computerSelection == 'Scissors') {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        alert(`You win! ${playerSelection} beats ${computerSelection}`);
     } else if (playerSelection === 'Paper' && computerSelection == 'Rock') {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        alert(`You win! ${playerSelection} beats ${computerSelection}`) ;
     } else if (playerSelection === 'Paper' && computerSelection == 'Scissors') {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        alert(`You lose! ${computerSelection} beats ${playerSelection}`);
     } else if (playerSelection === 'Scissors' && computerSelection == 'Paper') {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        alert(`You win! ${playerSelection} beats ${computerSelection}`) ;
     } else if (playerSelection === 'Scissors' && computerSelection == 'Rock') {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        alert(`You lose! ${computerSelection} beats ${playerSelection}`);
     }
 }
 
